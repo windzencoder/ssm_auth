dbd49e35adc2d9bbbb32a89d109ce1fe39109e80
wangzhen <869158130@qq.com>
2017-11-18 10:22:02 +0800

    搭建SpringMVC框架

---
+++ src/main/java/com/wz/controller/TestController.java
@@ -0,0 +1,21 @@
+package com.wz.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("/test")
+@Slf4j
+public class TestController {
+
+    @RequestMapping("/hello")
+    @ResponseBody
+    public String hello(){
+        log.info("hello");
+        return "hello, permissions";
+    }
+
+}
---
+++ src/main/java/com/wz/dao/TestDao.java
@@ -0,0 +1,4 @@
+package com.wz.dao;
+
+public class TestDao {
+}
