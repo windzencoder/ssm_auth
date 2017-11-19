5e334d70ad88007dd140468d6bb6d9ceb7fdcde7
wangzhen <869158130@qq.com>
2017-11-19 11:41:57 +0800

    工具类

---
+++ src/main/java/com/wz/common/ApplicationContextHelper.java
@@ -0,0 +1,33 @@
+package com.wz.common;
+
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.stereotype.Component;
+
+/**
+ * 获取ApplicationContext上下文
+ */
+@Component("applicationContextHelper")
+public class ApplicationContextHelper implements ApplicationContextAware {
+
+    private static ApplicationContext applicationContext;
+
+    public void setApplicationContext(ApplicationContext context) throws BeansException {
+        applicationContext = context;
+    }
+
+    public static <T> T popBean(Class<T> clazz) {
+        if (applicationContext == null) {
+            return null;
+        }
+        return applicationContext.getBean(clazz);
+    }
+
+    public static <T> T popBean(String name, Class<T> clazz) {
+        if (applicationContext == null) {
+            return null;
+        }
+        return applicationContext.getBean(name, clazz);
+    }
+}
