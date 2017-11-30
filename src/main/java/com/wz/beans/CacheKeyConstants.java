983844b5e0f9fbbef5893b21c8acde770e91521c
wangzhen <869158130@qq.com>
2017-11-30 17:27:55 +0800

    redis缓存

--- src/main/java/com/wz/beans/CacheKeyConstants.java
+++ src/main/java/com/wz/beans/CacheKeyConstants.java
@@ -5,8 +5,10 @@ import lombok.Getter;
 @Getter
 public enum CacheKeyConstants {
 
+    //系统内的所有权限
     SYSTEM_ACLS,
 
+    //用户权限
     USER_ACLS;
 
 }
---
+++ src/main/java/com/wz/service/SysCacheService.java
@@ -0,0 +1,68 @@
+package com.wz.service;
+
+import com.google.common.base.Joiner;
+import com.wz.beans.CacheKeyConstants;
+import com.wz.util.JsonMapper;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import redis.clients.jedis.ShardedJedis;
+
+import javax.annotation.Resource;
+
+/**
+ * 缓存服务
+ */
+
+@Service
+@Slf4j
+public class SysCacheService {
+
+    @Resource(name = "redisPool")
+    private RedisPool redisPool;
+
+    //保存系统的缓存
+    public void saveCache(String toSavedValue, int timeoutSeconds, CacheKeyConstants prefix) {
+        saveCache(toSavedValue, timeoutSeconds, prefix, null);
+    }
+
+    //保存
+    public void saveCache(String toSavedValue, int timeoutSeconds, CacheKeyConstants prefix, String... keys) {
+        if (toSavedValue == null) {
+            return;
+        }
+        ShardedJedis shardedJedis = null;
+        try {
+            String cacheKey = generateCacheKey(prefix, keys);
+            shardedJedis = redisPool.instance();
+            shardedJedis.setex(cacheKey, timeoutSeconds, toSavedValue);
+        } catch (Exception e) {
+            log.error("save cache exception, prefix:{}, keys:{}", prefix.name(), JsonMapper.obj2String(keys), e);
+        } finally {
+            redisPool.safeClose(shardedJedis);
+        }
+    }
+
+    public String getFromCache(CacheKeyConstants prefix, String... keys) {
+        ShardedJedis shardedJedis = null;
+        String cacheKey = generateCacheKey(prefix, keys);
+        try {
+            shardedJedis = redisPool.instance();
+            String value = shardedJedis.get(cacheKey);
+            return value;
+        } catch (Exception e) {
+            log.error("get from cache exception, prefix:{}, keys:{}", prefix.name(), JsonMapper.obj2String(keys), e);
+            return null;
+        } finally {
+            redisPool.safeClose(shardedJedis);
+        }
+    }
+
+    //生成缓存key
+    private String generateCacheKey(CacheKeyConstants prefix, String... keys) {
+        String key = prefix.name();
+        if (keys != null && keys.length > 0) {
+            key += "_" + Joiner.on("_").join(keys);
+        }
+        return key;
+    }
+}
